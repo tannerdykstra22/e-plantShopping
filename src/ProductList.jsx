7096cc080791a9c8f4bda7aa8870e0df01091078
tannerdykstra22 <tannerdykstra22@gmail.com>
2024-08-29 15:45:33 -0400

    task 1 one of final project completed

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,19 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import addItem from './CartSlice.jsx';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+           ...prevState,
+           [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+         }));
+      };
 
     const plantsArray = [
         {
